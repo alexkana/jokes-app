f0b08173b154220f5087839a4d284af53b5e2d6e
alexkana <alexkanakaris0@gmail.com>
2025-04-10 16:44:33 +0300

    Create a file for constants that are used in the app

---
+++ src/constants/index.ts
@@ -0,0 +1,30 @@
+/**
+ * Application-wide constants
+ */
+
+// API endpoints
+export const API_ENDPOINTS = {
+  RANDOM: 'https://official-joke-api.appspot.com/jokes/random',
+  PROGRAMMING: 'https://official-joke-api.appspot.com/jokes/programming/random',
+};
+
+// Local storage keys
+export const STORAGE_KEYS = {
+  SAVED_JOKES: 'savedJokes',
+};
+
+// Default values
+export const DEFAULTS = {
+  JOKE_RATING: 0,
+};
+
+// Animation timings (in ms)
+export const ANIMATION = {
+  TYPING_SPEED: 25,
+  FADE_DURATION: 500,
+}; 
+
+// Delay timings (in ms)
+export const DELAY = {
+  CLEAR_MESSAGE: 3000,
+};
