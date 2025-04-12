3217a4fb354afa703c68479ae06ebd408a5357fd
alexkana <alexkanakaris0@gmail.com>
2025-04-12 04:13:45 +0300

    Cursor not allowed when switch button is disabled

--- src/components/JokeCard.vue
+++ src/components/JokeCard.vue
@@ -44,10 +44,10 @@ onMounted(() => {
         </h2>
 
         <button
-          class="w-auto text-sm sm:text-base font-bold cursor-pointer px-3 py-1.5 sm:px-4 sm:py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 flex items-center justify-center"
+          class="w-auto text-sm sm:text-base font-bold px-3 py-1.5 sm:px-4 sm:py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 flex items-center justify-center"
           aria-label="Switch joke type"
           :disabled="isLoading"
-          :class="{ 'opacity-50 cursor-not-allowed': isLoading }"
+          :class="{ 'opacity-50 cursor-not-allowed': isLoading, 'cursor-pointer': !isLoading }"
           @click="toggleJokeType"
         >
           <Icon
